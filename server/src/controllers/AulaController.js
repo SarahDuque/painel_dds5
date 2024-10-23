d19899a8f8001b4cba53d8fa86782cec4eae4153
SARAH MONTEIRO DUQUE <s.duque@labcetec.net>
2024-10-23 16:24:11 -0300

    Web status e mensagens, tratando exceções

--- server/src/controllers/AulaController.js
+++ server/src/controllers/AulaController.js
@@ -1,4 +1,5 @@
 import { createAula, readAulas, updateAula, deleteAula, showOneAula } from "../models/AulaModel.js";
+import { hasProperty, isNullOrEmpty, verificaAula } from "../validations/AulaValidation.js";
 
 export async function criarAula(req, res) {
     console.log('AulaController criarAula');
@@ -7,14 +8,18 @@ export async function criarAula(req, res) {
     //exibindo corpo da requesição
     console.log(aula);
 
-    try {
-        const [status, resposta] = await createAula(aula);
-        res.status(status).json(resposta);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+    if (verificaAula(aula)) {
+        res.status(400).json({ menssage: 'Todas as propriedades devem ser preenchidas' });
     }
-
+    else {
+        try {
+            const [status, resposta] = await createAula(aula);
+            res.status(status).json(resposta);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
+    }
 }
 
 
@@ -43,13 +48,18 @@ export async function atualizarAula(req, res) {
     const aula = req.body;
     const { id } = req.params;
 
-    //Tentando atualizar aula
-    try {
-        const [status, resposta] = await updateAula(aula, id);
-        res.status(status).json(resposta)
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+    if (verificaAula(aula) || isNullOrEmpty(id)) {
+        res.status(400).json({ menssage: 'Todas as propriedades devem ser preenchidas' });
+    }
+    else {
+        //Tentando atualizar aula
+        try {
+            const [status, resposta] = await updateAula(aula, id);
+            res.status(status).json(resposta)
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
     }
 
 }
@@ -60,16 +70,23 @@ export async function excluirAula(req, res) {
 
     const { id } = req.params;
 
-    try {
-        const [status, resposta] = await deleteAula(id);
-        res.status(status).json(resposta);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+    if (verificaAula(aula)) {
+        res.status(400).json({ menssage: 'O id deve ser informado' });
     }
+    else {
+        try {
+            const [status, resposta] = await deleteAula(id);
+            res.status(status).json(resposta);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
+    }
+
+
 }
 
-export async function mostrarUmaAula(req,res) {
+export async function mostrarUmaAula(req, res) {
     console.log('AulaController mostrarUmaAula');
 
     const { id } = req.params;
@@ -81,5 +98,5 @@ export async function mostrarUmaAula(req,res) {
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
-    } 
+    }
 }
